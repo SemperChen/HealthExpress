6b80d936f4c67058bde1bbbb9abc8067a0ef64e1
SemperChen <semperchen@163.com>
2015-09-16 15:40:21 +0800

    first commit

---
+++ mobile/src/main/java/com/semperchen/healthexpress/mobile/ui/activity/MainActivity.java
@@ -0,0 +1,19 @@
+package com.semperchen.healthexpress.mobile.ui.activity;
+
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import com.semperchen.healthexpress.mobile.R;
+
+
+public class MainActivity extends AppCompatActivity{
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+}
